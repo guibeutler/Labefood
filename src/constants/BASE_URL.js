1e1894a1d69075c0b9d8c6f397da8e5f8752061a
Jônatas Felix <jon.cunha@hotmail.com>
2022-08-02 14:01:59 -0300

    alteração token

--- src/constants/BASE_URL.js
+++ src/constants/BASE_URL.js
@@ -2,6 +2,6 @@ export const BASE_URL = 'https://us-central1-missao-newton.cloudfunctions.net/ra
 
 export const token = {
     headers:{
-        auth: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImpiVDUzTThOQ1hjVGNoaXRiMUF5IiwibmFtZSI6IkxvcnJhbiIsImVtYWlsIjoibG9ycmFuQG91dGxvb2suY29tIiwiY3BmIjoiMTIzLjk4Ny40NTYtNjUiLCJoYXNBZGRyZXNzIjp0cnVlLCJhZGRyZXNzIjoiUi4gQWZvbnNvIEJyYXosIDE3NywgNzEgLSBWaWxhIE4uIENvbmNlacOnw6NvIiwiaWF0IjoxNjU5MzgwMjg0fQ.Pq7MJ8BJsub_nfjnATCzkZMVX8qdAFPpFtRnDxM_YGg",
+        auth: localStorage.getItem('token'),
     }
 } 
