6c2490c9cbd66ddfef09a8a38f3a892226b65ef4
Lorran Christo dos Santos <lorransantos99@outlook.com>
2022-08-02 22:38:10 -0300

    Corrigido alinhamento do loader do feed

---
+++ src/components/LoaderCard/LoaderCard.js
@@ -0,0 +1,13 @@
+import { Container, Loader, Card } from './style';
+
+function LoaderCard() {
+  return (
+    <Container>
+      <Card>
+        <Loader />
+      </Card>
+    </Container>
+  );
+}
+
+export default LoaderCard;
