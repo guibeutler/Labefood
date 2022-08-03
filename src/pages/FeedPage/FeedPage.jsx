8fa2488d0310ffda5a1d7887130ce1e751400c34
Guilherme Beutler <guilherme.beutler@outlook.com>
2022-08-03 14:22:26 -0300

    Pedido em andamento

--- src/pages/FeedPage/FeedPage.jsx
+++ src/pages/FeedPage/FeedPage.jsx
@@ -14,6 +14,7 @@ import {
   Button,
   Category,
   ContainerLoader,
+  Search,
 } from "./style";
 import CardRestaurantFeed from "../../components/CardRestaurantFeed/CardRestaurantFeed";
 import CardFilterFeed from "../../components/CardFilterFeed/CardFilterFeed";
@@ -22,12 +23,15 @@ import Header from "../../components/Header/Header";
 import GoToTop from "../../components/GoToTop/GoToTop";
 import LoaderCard from "../../components/LoaderCard/LoaderCard";
 import FooterNavigation from '../../components/Footer/Footer';
+import OrderActive from "./OrderActive";
+import useRequestData from '../../hooks/useRequestData';
 
 
 export default function FeedPage() {
   const { states, setters } = useContext(GlobalContext);
   const [category, setCategory] = useState("");
   const categoryBar = useRef(null);
+  const getActiveOrder = useRequestData({}, `${BASE_URL}/active-order`)
 
   const navigate = useNavigate()
 
@@ -115,7 +119,6 @@ export default function FeedPage() {
         </Button>
       </CategoryNavBar>
 
-      {category ? (
 
 
       {!states.loaderCard ? (
@@ -131,6 +134,12 @@ export default function FeedPage() {
         <CardRestaurantFeed />
       )}
       <GoToTop />
+      {getActiveOrder.order &&
+                (<OrderActive
+                    restaurantName={getActiveOrder.order.restaurantName}
+                    totalPrice={getActiveOrder.order.totalPrice}
+                />)
+            }
       <FooterNavigation/>
     </Container>
   );
